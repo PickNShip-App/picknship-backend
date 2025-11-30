81230b7d0395776e3a719605d03f8e8eb1a77cfa
Agustin Paturlanne <aguspaturlanne@gmail.com>
2025-11-30 11:51:58 +1100

    Add rates router to main application and update stores router import

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
-from app.api import auth, webhook
-from app.api.stores import router as stores_router
+from app.api import auth, webhook, stores, rates
+
 
 app = FastAPI(title="Pick'NShip API")
 
 app.include_router(auth.router)
 app.include_router(webhook.router)
-app.include_router(stores_router)
+app.include_router(stores.router)
+app.include_router(rates.router)
 
 @app.get("/")
 def home():
